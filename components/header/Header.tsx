48cf326787a5d9fe48dbd634db3add0255d729cd
Alexsander Silva <alexsander.silva14@live.com>
2022-10-07 16:12:15 -0300

    Aprendendo a utilizar import

---
+++ components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <header>
+        <div>Logotipo</div>
+        <nav>Navegação Menu</nav>
+        <div>Caixa de Busca</div>
+    </header>
+    
+  )
+}
---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/header'
+
+export default function LoginPage() {
+  return (
+    <>
+      <Header />
+      <h1>Seja bem vindo ao React</h1>
+    </>
+  )
+}
